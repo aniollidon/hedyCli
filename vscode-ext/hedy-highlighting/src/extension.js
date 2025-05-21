4a26e4e5df3252cfbf57ad5bd6153abba30bb8b5
Aniol Lidon <alidon1@xtec.cat>
2025-05-21 23:56:10 +0200

    v3! side view

--- vscode-ext/hedy-highlighting/src/extension.js
+++ vscode-ext/hedy-highlighting/src/extension.js
@@ -37,3 +37,73 @@ vscode.workspace.onDidChangeConfiguration(e => {
     }, 50)
   }
 })
+
+vscode.commands.registerCommand('hedy.showPreviewToSide', () => {
+  const editor = vscode.window.activeTextEditor
+  if (!editor) {
+    vscode.window.showErrorMessage('Cap document actiu')
+    return
+  }
+
+  const name = editor.document.fileName.split(/[\/\\]/).pop()
+  // extensió (.hy1, .hy2, .hy3... .hy17) només el numero
+  const hedyLevel = name.match(/\.hy(\d+)/)
+  if (!hedyLevel) {
+    vscode.window.showErrorMessage(name + ' is not a valid Hedy file')
+    return
+  }
+
+  const panel = vscode.window.createWebviewPanel('previewView', `preview: ${name}`, vscode.ViewColumn.Beside, {
+    enableScripts: true,
+  })
+
+  // Get current document
+  const document = editor.document
+  const text = document.getText()
+  const url = `https://www.hedy.org/render_code/${hedyLevel[1]}/?code=${encodeURIComponent(text)}#start`
+
+  // Actualitzar el contingut de la vista prèvia quan es canvia el document
+  const updatePreview = () => {
+    const text = document.getText()
+    const url = `https://www.hedy.org/render_code/${hedyLevel[1]}/?code=${encodeURIComponent(text)}#start`
+    panel.webview.html = `
+      <html>
+        <head>
+          <style>
+            body, html {
+              margin: 0;
+              padding: 0;
+              width: 100%;
+              height: 100%;
+              overflow: hidden;
+            }
+            .iframe-container {
+              width: 100%;
+              height: 100%;
+              transform-origin: 0 0;
+              transform: scale(0.8); /* Zoom out to 80% */
+            }
+            iframe {
+              width: 125%; /* Compensate for the scale */
+              height: 125%; /* Compensate for the scale */
+              border: none;
+            }
+          </style>
+        </head>
+        <body>
+          <div class="iframe-container">
+            <iframe src="${url}"></iframe>
+          </div>
+        </body>
+      </html>
+    `
+  }
+  // Actualitzar la vista prèvia quan el document canvia
+  const disposable = vscode.workspace.onDidChangeTextDocument(event => {
+    if (event.document === document) {
+      updatePreview()
+    }
+  })
+
+  updatePreview()
+})
